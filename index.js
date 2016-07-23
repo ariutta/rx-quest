7eab1a449c1e24c383af7e891418aa6acd60f560
Anders Riutta <git@andersriutta.com>
2016-07-23 03:18:34 -0700

    It's now expanded to cover more superagent functionality in the web worker.

--- index.js
+++ index.js
@@ -1,26 +1,84 @@
-var superagent = window.superagent || {};
-if (!window.superagent) {
-  window.superagent = superagent;
-}
+var Emitter = require('component-emitter');
 
-superagent.getChunked = function(input, next, error, complete) {
+var Superagent = function() {
+  var args = Array.prototype.slice.call(arguments);
+
+  this.callstack = [];
+
+  if (args.length) {
+    var callElement = {};
+    callElement.name = 'get';
+    callElement.args = args;
+    this.callstack.push(callElement);
+  }
+
+};
+
+var methods = [
+  'post',
+  'put',
+  'get',
+  'accept',
+  'send',
+  'set',
+  'head',
+  'del',
+  'query',
+  'type',
+  'serialize',
+  'buffer',
+  'parse',
+  'attach',
+  'field',
+  'withCredentials',
+  //'on',
+  'auth',
+  'timeout',
+];
+
+methods.forEach(function(method) {
+  Superagent.prototype[method] = function(name) {
+    var args = Array.prototype.slice.call(arguments);
+    var callElement = {};
+    callElement.name = method;
+    callElement.args = args;
+    this.callstack.push(callElement);
+    console.log('callstack');
+    console.log(this.callstack);
+    return this;
+  };
+});
+
+Superagent.prototype.end = function() {
+  var that = this;
+  var callstack = that.callstack;
+
   var requestWorker = new Worker('superagent-get-chunked-worker.min.js');
   requestWorker.onmessage = function(oEvent) {
     var data = oEvent.data;
     var type = data.type;
     var body = data.body;
     if (type === 'next') {
-      next(body);
+      that.emit('data', body);
     } else if (type === 'complete') {
-      complete();
+      that.emit('end', body);
     }
   };
 
-  requestWorker.onerror = error;
+  requestWorker.onerror = function(err) {
+    that.emit('error', err);
+  };
 
   requestWorker.postMessage({
-    input: input
+    callstack: callstack
   });
 };
 
-module.exports = superagent;
+Superagent.prototype.then = Superagent.prototype.end;
+
+Emitter(Superagent.prototype);
+
+var request = new Superagent();
+
+window.request = request;
+module.exports = request;
--- superagent-get-chunked.js
+++ superagent-get-chunked.js
@@ -1,56 +1,118 @@
 var superagent = require('superagent');
 
+//var chunkedRequest = require('chunked-request');
+//
+//chunkedRequest({ 
+//  url: 'http://my.api/endpoint',
+//  method: 'POST',
+//  headers: { /*...*/ },
+//  body: JSON.stringify({ /*...*/ }),
+//  credentials: 'include',
+//  chunkParser(rawChunk) { /*...*/ },
+//  onChunk(err, parsedChunk) { /*...*/ },
+//  onComplete(result) { /*...*/ }
+//});
+
 function noop() {}
 
-var input = e.data.input;
+var callstack = e.data.callstack;
 
-var XHR = 'xhr';
+console.log('callstack');
+console.log(callstack);
 
-var index = 0;
+function get(input) {
+  console.log('input24');
+  console.log(input);
+  var XHR = 'xhr';
 
-var myresponse = superagent.get(input, noop);
-var xhr = myresponse.xhr;
+  var index = 0;
 
-// drawing on code from here:
-// https://github.com/jonnyreeves/chunked-request/blob/master/src/impl/xhr.js
-function onProgressEvent() {
-  var rawChunk = xhr.responseText.substr(index);
-  index = xhr.responseText.length;
-  postMessage({
-    body: rawChunk,
-    type: 'next'
-  });
+  var myresponse = superagent.get(input, noop);
+  var xhr = myresponse.xhr;
+
+  // drawing on code from here:
+  // https://github.com/jonnyreeves/chunked-request/blob/master/src/impl/xhr.js
+  function onProgressEvent() {
+    var rawChunk = xhr.responseText.substr(index);
+    console.log('rawChunk38');
+    console.log(rawChunk);
+    index = xhr.responseText.length;
+    postMessage({
+      body: rawChunk,
+      type: 'next'
+    });
+  }
+
+  function onLoadEvent() {
+    var xhrJSON = JSON.parse(JSON.stringify(xhr));
+    var message = {
+      statusCode: xhr.status,
+      transport: XHR,
+      raw: xhrJSON
+    };
+    postMessage({
+      body: message,
+      type: 'completed'
+    });
+  }
+
+  function onError(err) {
+    console.error('err56');
+    console.error(err);
+    // TODO this seems odd
+    err = JSON.parse(JSON.stringify(err));
+    var message = {
+      statusCode: 0,
+      transport: XHR,
+      raw: err
+    };
+    postMessage({
+      body: message,
+      type: 'error'
+    });
+    // TODO does this happen automatically?
+    close();
+  }
+
+  xhr.addEventListener('progress', onProgressEvent);
+  xhr.addEventListener('loadend', onLoadEvent);
+  xhr.addEventListener('error', onError);
 }
 
-function onLoadEvent() {
-  var xhrJSON = JSON.parse(JSON.stringify(xhr));
-  var message = {
-    statusCode: xhr.status,
-    transport: XHR,
-    raw: xhrJSON
-  };
+function endCb(result) {
+  var resultJSON = JSON.parse(JSON.stringify(result));
   postMessage({
-    body: message,
+    body: resultJSON,
     type: 'completed'
   });
 }
 
-function onError(err) {
-  // TODO this seems odd
-  var err = JSON.parse(JSON.stringify(err));
-  var message = {
-    statusCode: 0,
-    transport: XHR,
-    raw: err
-  };
-  postMessage({
-    body: message,
-    type: 'error'
-  });
-  // TODO does this happen automatically?
-  close();
-}
+//superagent.on('error', function onError(err) {
+//  // TODO this seems odd
+//  err = JSON.parse(JSON.stringify(err));
+//  var message = {
+//    statusCode: 0,
+//    raw: err
+//  };
+//  postMessage({
+//    body: message,
+//    type: 'error'
+//  });
+//  // TODO does this happen automatically?
+//  close();
+//});
 
-xhr.addEventListener('progress', onProgressEvent);
-xhr.addEventListener('loadend', onLoadEvent);
-xhr.addEventListener('error', onError);
+var callstackNames = callstack.map(function(callElement) {
+  return callElement.name;
+});
+callstack.forEach(function(callElement) {
+  var name = callElement.name;
+  var args = callElement.args;
+  if (name === 'get') {
+    get(args);
+  } else if (['end', 'then'].indexOf(name) > -1 && callstackNames.indexOf('get') === -1) {
+    superagent.end(endCb);
+  } else {
+    superagent[name].apply(null, args);
+  }
+});
